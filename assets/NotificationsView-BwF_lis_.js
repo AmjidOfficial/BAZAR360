f3fbb410096c976f4517ef658683f68823892554
Muhammad Amjid <amjid.bisconni@gmail.com>
2026-07-15 22:45:23 +0500

    Updates

--- assets/NotificationsView-BwF_lis_.js
+++ assets/NotificationsView-BwF_lis_.js
@@ -1,4 +1,4 @@
-import{c as a,j as e,w as o,H as s,l}from"./index-CV7nUInb.js";/**
+import{i as a,j as e,P as o,H as s,f as l}from"./index-CAAGF97U.js";/**
  * @license lucide-react v0.546.0 - ISC
  *
  * This source code is licensed under the ISC license.
