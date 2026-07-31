d0e98233084c9203305378700af02077ae873f6a
Ghani Khan <khattakghani94@gmail.com>
2026-07-31 19:45:58 +0500

    Updates

--- assets/permissions-BUcsQ1nr.js
+++
@@ -1,6 +0,0 @@
-import{c as n}from"./index-CXqydb0_.js";/**
- * @license lucide-react v0.546.0 - ISC
- *
- * This source code is licensed under the ISC license.
- * See the LICENSE file in the root directory of this source tree.
- */const c=[["circle",{cx:"12",cy:"12",r:"10",key:"1mglay"}],["path",{d:"m15 9-6 6",key:"1uzhvr"}],["path",{d:"m9 9 6 6",key:"z0biqf"}]],d=n("circle-x",c),t=["amjid.bisconni@gmail.com","mazharsouls@gmail.com","khattakghani94@gmail.com"],o=["Muhammad Amjid","Malak Mazhar","Ghani Khan"];function m(e){if(!e)return!1;const a=(e.email||"").toLowerCase(),i=e.displayName||"";return!!(t.includes(a)||o.some(r=>i.toLowerCase().includes(r.toLowerCase())))}function l(e,a){return e?m(e)?!0:a?a.createdBy===e.uid||a.dealerId===e.uid||a.ownerId===e.uid:!1:!1}export{d as C,l as c,m as i};
