41d7ffed63b56514c6a116296641f0f49de86c15
Muhammad Amjid <amjid.bisconni@gmail.com>
2026-08-23 14:41:00 +0500

    Updates

--- assets/trash-DJI1LyJf.js
+++ assets/trash-DJI1LyJf.js
@@ -1,4 +1,4 @@
-import{c as a}from"./index-CK0rNSM1.js";/**
+import{c as a}from"./index-Djl0LD2s.js";/**
  * @license lucide-react v0.546.0 - ISC
  *
  * This source code is licensed under the ISC license.
